057bf4f38840e70080eb280e13420e6aea8c376e
kck1337 <92188404+kck1337@users.noreply.github.com>
2022-06-28 11:08:39 -0400

    Servo_sweep

---
+++ Servo/Servo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>    // Include servo library
+
+Servo myservo;        // Create servo object "myservo"
+
+int potPin = A0;      // Define pin for the potentiometer  
+int val = 0;          // Initialzie variable
+
+
+void setup() {
+  myservo.attach(9);  // Assign a pin number to the servo object  
+  
+}
+
+void loop() {
+  for(int pos = 0; pos <- 180; pos++){       // Sweep servo from 0 to 180 degrees
+    myservo.write(pos);                      // write servo position 
+    delay(15);                               // Pause 15 ms
+    }
+  for(int pos = 180; pos >= 0; pos--){
+    myservo.write(pos);
+    delay(15);
+    }
+}
