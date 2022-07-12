05beb73b3e4c1cdc5596edd1f35e83dea9e556a7
kck1337 <92188404+kck1337@users.noreply.github.com>
2022-07-12 12:58:01 -0400

    Update RGB_LED.ino

--- RGB_LED/RGB_LED.ino
+++ RGB_LED/RGB_LED.ino
@@ -8,20 +8,23 @@ void setup() {
   pinMode(Red, OUTPUT);
   pinMode(Green, OUTPUT);
   pinMode(Blue, OUTPUT);
+  digitalWrite(Red, HIGH);
+  digitalWrite(Green, HIGH);
+  digitalWrite(Blue, HIGH);
 }
 
 // Turn each colour on for 1 second and off in 1/4th of second
 void loop() {
-  digitalWrite(Red, HIGH);
-  delay(1000);
   digitalWrite(Red, LOW);
-  delay(250);
-  digitalWrite(Green, HIGH);
-  delay(1000);
+  delay(1);
+  digitalWrite(Red, HIGH);
+  delay(1);
   digitalWrite(Green, LOW);
-  delay(250);
-  digitalWrite(Blue, HIGH);
-  delay(1000);
+  delay(1);
+  digitalWrite(Green, HIGH);
+  delay(1);
   digitalWrite(Blue, LOW);
-  delay(250);
+  delay(1);
+  digitalWrite(Blue, HIGH);
+  delay(1);
 }
