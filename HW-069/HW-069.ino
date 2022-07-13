0b4bfea5c4d4f10f825cc83bdd3ca28175cbef63
kck1337 <92188404+kck1337@users.noreply.github.com>
2022-07-12 20:29:25 -0400

    Grove 7 segment LED

---
+++ HW-069/HW-069.ino
@@ -0,0 +1,40 @@
+#include <TM1637.h>
+
+int CLK = 2;
+int DIO = 3;
+
+TM1637 tm(CLK,DIO);
+
+void setup() {
+  // put your setup code here, to run once:
+  tm.init();
+
+  //set brightness; 0-7
+  tm.set(2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // example: "12:ab"
+  // tm.display(position, character);
+  tm.display(0,12);
+  tm.display(1,13);
+  tm.point(1);
+  tm.display(2,14);
+  tm.display(3,15);
+
+  delay(3000);
+
+  // example: "1234"
+  displayNumber(1234);
+
+  delay(1000);
+}
+
+void displayNumber(int num){   
+    tm.display(3, num % 10);   
+    tm.display(2, num / 10 % 10);   
+    tm.display(1, num / 100 % 10);   
+    tm.display(0, num / 1000 % 10);
+}
---
+++ TWI_7SEg/TWI_7SEg.ino
@@ -0,0 +1,22 @@
+#include <TM1637.h>
+int CLK = 2;
+int DIO = 3;
+
+TM1637 tm(CLK, DIO);
+
+void setup() {
+//nitialize display
+  tm.init();
+  tm.set(7);
+}
+
+void loop() {
+for(int i = 0; i<10000;i++) displayNumber(i) ;
+}
+
+void displayNumber(int num){   
+    tm.display(3, num % 10);   
+    tm.display(2, num / 10 % 10);   
+    tm.display(1, num / 100 % 10);   
+    tm.display(0, num / 1000 % 10);
+}
