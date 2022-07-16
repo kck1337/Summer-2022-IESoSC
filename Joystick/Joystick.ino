64939a4a598f99c9b01900338afc8541e4054815
kck1337 <92188404+kck1337@users.noreply.github.com>
2022-07-16 19:35:44 -0400

    Update Joystick.ino

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -13,9 +13,9 @@ void loop() {
   int x = analogRead(A0);
   int y = analogRead(A1);
   int dr = map(x, 0, 1023, -100, 100);
-  dr = dr * 0.4 + (data1 * 0.6);
+//  dr = dr * 0.4 + (data1 * 0.6);
   int tr = map(y, 0, 1023, -100, 100);
-  tr = tr * 0.4 + (data2 * 0.6);
+//  tr = tr * 0.4 + (data2 * 0.6);
 //  int vec = vector(dr, tr);
   Serial.print(dr);
   Serial.print(",");
