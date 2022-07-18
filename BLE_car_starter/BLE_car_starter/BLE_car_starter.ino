cca7ed802c57c5a5b49b2806b3ddbd3bbbe8b97a
kck1337 <92188404+kck1337@users.noreply.github.com>
2022-07-18 14:05:31 -0400

    Create BLE_car_starter.ino

---
+++ BLE_car_starter/BLE_car_starter/BLE_car_starter.ino
@@ -0,0 +1,89 @@
+#include <ArduinoBLE.h>
+#include <Servo.h>
+
+//on terminal, use [uuidgen] **On Mac or Linux** to get a randomly-generated ID
+//use the ID as serviceUUID and change 1 character and use as charUUID
+//copy the serviceUUID into the serviceUUID JavaScript variable as well
+
+#define serviceUUID "091c623a-06c1-11ed-b939-0242ac120002"
+#define charUUID "191c623a-06c1-11ed-b939-0242ac120002"
+
+//String carName = "Hehe"; //name your car whatever you want
+
+BLEService moveCar(serviceUUID);
+BLEIntCharacteristic dir(charUUID, BLERead | BLEWrite);
+
+int lm = 2; // Motor pins
+int rm = 3;
+
+Servo left; // Can change
+Servo right;
+
+void setup() {
+  left.attach(2); // can change 
+  right.attach(3);
+  left.write(90);
+  right.write(90);
+//  Serial.begin(9600);
+//  while (!Serial) {};
+  while (!BLE.begin()) {
+    Serial.println("Error initializing BLE...");
+    delay(2000);
+  }
+//  Serial.println("BLE initialized!");
+  BLE.setLocalName("Hehe"); 
+  BLE.setAdvertisedService(moveCar);
+  moveCar.addCharacteristic(dir);
+  BLE.addService(moveCar);
+  BLE.advertise();
+}
+
+bool centralConnected = false;
+
+void loop() {
+  BLEDevice central = BLE.central();
+  if (central && !centralConnected) {
+//    Serial.print("Connected to central: ");
+//    Serial.println(central.address());
+    centralConnected = true;
+  } else if (!central && centralConnected) {
+//    Serial.println("Disconnected from central.");
+    centralConnected = false;
+  }
+
+  if (central.connected() && central) {
+    if (dir.written()) {
+//      Serial.println("BLE received:");
+//      Serial.println(dir.value());
+      int i = dir.value();
+      drive(i);
+    }
+  }
+}
+
+void drive(int x) {
+  if(x == 0){
+    left.write(90);
+    right.write(90);
+    }
+  else if (x == 1) {
+    left.write(100);
+    right.write(80);
+  }
+  else if (x == 2) {
+    left.write(80);
+    right.write(100);
+  }
+  else if(x == 3){
+    left.write(80);
+    right.write(80);    
+    }
+  else if(x == 4){
+    left.write(100);
+    right.write(100); 
+    }
+  else{
+    left.write(90);
+    right.write(90);
+    }
+}
